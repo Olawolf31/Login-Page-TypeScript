623ade6cef9bf4cc9b1da49ffaafa878a58574cf
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-01-27 09:51:13 +0100

    env update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React, {useState} from 'react';
-import Login from './components/Login';
-
+import React, { useState } from "react";
+import Login from "./components/userLogin/Login";
 
 const App: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  console.log(username)
+  /*   const domain = process.env.AUTH0_DOMAIN || ''
+  const clientId = process.env.AUTH0_CLIENT_ID || '' */
 
   return (
     <div>
-      <Login 
-        username={username} 
+      <Login
+        username={username}
         setUsername={setUsername}
-        password={password} 
-        setPassword={setPassword} 
+        password={password}
+        setPassword={setPassword}
       />
     </div>
   );
-}
+};
 
 export default App;
