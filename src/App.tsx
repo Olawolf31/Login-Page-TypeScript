6c281d37114a10c50782ce83dd362354abc3b93f
Olawolf31 <110026445+Olawolf31@users.noreply.github.com>
2023-01-27 10:01:15 +0100

    App cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ const App: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  /*   const domain = process.env.AUTH0_DOMAIN || ''
-  const clientId = process.env.AUTH0_CLIENT_ID || '' */
+
 
   return (
     <div>
